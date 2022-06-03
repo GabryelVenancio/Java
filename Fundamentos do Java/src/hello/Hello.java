c69eeb561156d6d7dce8dcb718403582a24d822b
GabryelVenancio <88159869+GabryelVenancio@users.noreply.github.com>
2022-06-03 16:39:15 -0300

    Add files via upload

---
+++ Fundamentos do Java/src/hello/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * Hello World
+ * @author Gabryel
+ */
+package hello;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Classe Principal - Hello World+
+ * 
+ * @author gabryel.vcnascimento
+ *
+ */
+
+public class Hello {
+	/**
+	 * Método principal - Hello World
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		JOptionPane.showMessageDialog(null, "Hello World");
+	}
+
+}
