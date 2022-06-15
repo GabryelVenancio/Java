ecc33d0073d1280fe3bbe6004f115e73de2d5dc6
GabryelVenancio <88159869+GabryelVenancio@users.noreply.github.com>
2022-06-15 17:26:38 -0300

    Add files via upload

---
+++ carro/Aviao.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package carro;
+
+/**
+ * @author gabryel
+ * Classe Modelo com herança (extends)
+ */
+public class Aviao extends Carro {
+
+	/**
+	 * Construtor
+	 */
+	public Aviao() {
+		System.out.println("___ Aeronaves ___");
+	}
+	
+	double envergadura;
+	public String aterrizar;
+
+	/**
+	 * @param args
+	 */
+	void aterrizar() {
+		System.out.println("Aterrissagem: Liberada");
+	}
+
+}
---
+++ carro/Carro.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package carro;
+
+/**
+ * @author gabryel
+ *
+ */
+
+public class Carro {
+
+	/**
+	 * Construtor
+	 */
+	public Carro() {
+		super();
+		System.out.println("------------------------------");
+	}
+	String modelo;
+	int ano;
+	String cor;
+	
+	void ligar () {
+		System.out.println("Ligando Carro");
+	}
+	
+	void desligar() {
+		System.out.println("Desligando Carro");
+	}
+	
+	void acelerar() {
+		System.out.println("Acelerando carro");
+	}
+}
